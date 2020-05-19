5c380b455af0f60dd1a459d89b627ed3d01d88b8
DevMofu <DevMoFu>
2020-05-19 18:08:43 -0400

    Adding setFanSpeed, has been tested

--- main.go
+++ main.go
@@ -37,9 +37,16 @@ func ipmiBoilerplate(args string, algoName string) {
 //}
 
 // works
-func checkSystemTemps(c string) {
-	algoName := "checkSystemTemps"
+func checkSystemSensors(c string) {
+	algoName := "checkSystemSensors"
 	args := fmt.Sprintf("%vsensor", c)
+	ipmiBoilerplate(args, algoName)
+}
+
+func checkSystemTemps(c string) {
+	//  sensor reading 'Inlet Temp' Temp
+	algoName := "checkSystemTemps"
+	args := fmt.Sprintf("%vsensor Temp", c)
 	ipmiBoilerplate(args, algoName)
 }
 
@@ -58,7 +65,7 @@ func checkThirdPartyCardBehavior(c string) {
 	ipmiBoilerplate(args, algoName)
 }
 
-//WIP
+//Works
 func setManualFanMode(c string, ManualFanMode string) {
 	algoName := "setManualFanMode"
 	var ManualFanModeHex string
@@ -84,54 +91,61 @@ func setManualFanMode(c string, ManualFanMode string) {
 	ipmiBoilerplate(args, algoName) // s
 }
 
-//func setFanSpeed(FanSpeed int) {
-//	algoName := "setFanSpeed"
-//	// fan speed
-//	var fanSpeedHex string
-//	switch FanSpeed {
-//	case 10:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x0a"
-//	case 15:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x0f"
-//	case 20:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x14"
-//	case 25:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x19"
-//	case 30:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x1e"
-//	case 35:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x23"
-//	case 40:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x28"
-//	case 45:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x2d"
-//	case 50:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x32"
-//	case 55:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x37"
-//	case 60:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x3c"
-//	case 65:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x41"
-//	case 70:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x46"
-//	case 75:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x4b"
-//	case 80:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x50"
-//	case 85:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x55"
-//	case 90:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x5a"
-//	case 95:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x5f"
-//	case 100:
-//		fanSpeedHex = "0x30 0x30 0x02 0xff 0x5a"
-//	default:
-//		fmt.Printf("Null or no valid 3rd Party Behavior selected. No action taken", algoName)
-//	}
-//}
+func setFanSpeed(c string, FanSpeed int) {
+	algoName := "setFanSpeed"
+	var fanSpeedHex string
+	switch FanSpeed {
+	case 10:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x0a"
+	case 15:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x0f"
+	case 20:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x14"
+	case 25:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x19"
+	case 30:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x1e"
+	case 35:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x23"
+	case 40:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x28"
+	case 45:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x2d"
+	case 50:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x32"
+	case 55:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x37"
+	case 60:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x3c"
+	case 65:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x41"
+	case 70:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x46"
+	case 75:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x4b"
+	case 80:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x50"
+	case 85:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x55"
+	case 90:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x5a"
+	case 95:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x5f"
+	case 100:
+		fanSpeedHex = "0x30 0x30 0x02 0xff 0x5a"
+	default:
+		fmt.Printf("%v: '%s' is not a valid value. Try 'enable' or 'disable\n", FanSpeed, algoName)
+	}
 
+	// TODO: Notify user of fan mod change
+	setManualFanMode(c, "enable")
+
+	args := fmt.Sprintf("%s%s %s", c, "raw", fanSpeedHex)
+	println(args)
+	ipmiBoilerplate(args, algoName)
+}
+
+// working
 func setThirdPartyCardBehavior(c string, thirdPartyCardBehavior string) {
 	algoName := "setThirdPartyCardBehavior"
 	var thirdPartyCardBehaviorHex string
@@ -147,7 +161,7 @@ func setThirdPartyCardBehavior(c string, thirdPartyCardBehavior string) {
 	}
 
 	args := fmt.Sprintf("%s%s %s", c, "raw", thirdPartyCardBehaviorHex)
-	println(args)
+	//println(args) //test
 	ipmiBoilerplate(args, algoName)
 }
 
@@ -157,6 +171,7 @@ type creds struct {
 	password   string
 }
 
+// TODO: Add logic for option selection
 func main() {
 
 	// credential
@@ -171,49 +186,23 @@ func main() {
 
 	c := creds{*hostnameIP, *username, *password}
 	credString := fmt.Sprintf("-I lanplus -H %v -U %v -P %v ", c.hostnameIP, c.username, c.password)
+
 	//checkSystemTemps(credString)
 	//checkCurrentFanSpeed(credString)
 	checkThirdPartyCardBehavior(credString)
 	//setManualFanMode(credString, *ManualFanMode)
-	setThirdPartyCardBehavior(credString, *thirdPartyCardBehavior)
+	//setThirdPartyCardBehavior(credString, *thirdPartyCardBehavior)
+	setFanSpeed(credString, *FanSpeed)
 
+	// Silence Go
 	fmt.Sprintln(ManualFanMode, FanSpeed, thirdPartyCardBehavior) // remove after testing
 
 }
 
 /*
-		This command will print information about the System Temperature and FAN RPMs.
-
-	    ipmitool -I lanplus -H yourIPAddress -U yourUsername -P yourPassword sensor reading "Temp" 'Fan1A' 'Fan1B' 'Fan2A' 'Fan2B' 'Fan3A' 'Fan3B' 'Fan4A' 'Fan4B'
-
 	Note: “TEMP” was the name of my sensor, You can try “Ambient Temperature” for your server if you want to see the temperature of the CPU.  Also, the fan names are case sensitive.  So save yourself a few moments of troubleshooting by using the name as reported in iDRAC.
 	This command will print out a ton of information about the Fans, stats for nerds basically.
 
-		ipmitool -I lanplus -H yourIPAddress -U yourUsername -P yourPassword sdr get 'Fan1A' 'Fan1B' 'Fan2A' 'Fan2B' 'Fan3A' 'Fan3B' 'Fan4A' 'Fan4B'
-
-		ipmitool -I lanplus -H yourIPAddress -U yourUsername -P yourPassword raw
-
-*/
-
-/*
 https://github.com/ipmitool/ipmitool/issues/30
 https://www.reddit.com/r/homelab/comments/7xqb11/dell_fan_noise_control_silence_your_poweredge/
-
-# print temps and fans rpms
-ipmitool -I lanplus -H <iDRAC-IP> -U <iDRAC-USER> -P <iDRAC-PASSWORD> sensor reading "Ambient Temp" "FAN 1 RPM" "FAN 2 RPM" "FAN 3 RPM"
-#
-# print fan info
-ipmitool -I lanplus -H <iDRAC-IP> -U <iDRAC-USER> -P <iDRAC-PASSWORD> sdr get "FAN 1 RPM" "FAN 2 RPM" "FAN 3 RPM"
-#
-# enable manual/static fan control
-ipmitool -I lanplus -H <iDRAC-IP> -U <iDRAC-USER> -P <iDRAC-PASSWORD> raw 0x30 0x30 0x01 0x00
-#
-# disable manual/static fan control
-ipmitool -I lanplus -H <iDRAC-IP> -U <iDRAC-USER> -P <iDRAC-PASSWORD> raw 0x30 0x30 0x01 0x01
-#
-
-Function 	ipmitool raw command
-Disable 3rd Party Card fan behavior 	ipmitool raw 0x30 0xce 0x00 0x16 0x05 0x00 0x00 0x00 0x05 0x00 0x01 0x00 0x00
-Enable 3rd Party Card fan behavior 	ipmitool raw 0x30 0xce 0x00 0x16 0x05 0x00 0x00 0x00 0x05 0x00 0x00 0x00 0x00
-Check 3rd Party Card fan behavior 	ipmitool raw 0x30 0xce 0x01 0x16 0x05 0x00 0x00 0x00
 */
